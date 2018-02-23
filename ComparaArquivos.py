d04225d4332b23866fecaa6e84456331749ac028
Alex Piter <alexsoares2517@hotmail.com>
2018-02-23 10:12:33 -0300

    corrigido comentário

--- ComparaArquivos.py
+++ ComparaArquivos.py
@@ -2,10 +2,9 @@ import glob
 import os
 
 dwg = glob.glob(input('Escolha a extensão que deseja filtrar: '))  # Filtra apenas os arquivos com a extensão inserida e salva numa lista
-# with open() as data:  # Abre o arquivo sobs.txt contendo parte da string a ser comparada
 for root, dirs, files in os.walk('.'):
     for file in files:
-        if file.endswith(".txt"):
+        if file.endswith(".txt"): # Abre qualquer arquivo txt contendo a string a ser comparada
             with open(file) as data:
                 datalines = (line.rstrip('\r\n') for line in data)  # Separa cada sob como uma nova linha no arquivo txt
                 for line in datalines:  # Faz uma varretura linha a linha no arquivo txt
